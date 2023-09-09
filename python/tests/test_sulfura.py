d16123037d52b3e99674d1418acb9f3dfaeb3db6
Rafael Penna <rafaelgpenna@gmail.com>
2023-09-09 20:49:42 -0300

    test: sulfuras quality behavior

---
+++ python/tests/test_sulfura.py
@@ -0,0 +1,15 @@
+import pytest
+from gilded_rose import Item, GildedRose, SULFURAS
+
+
+DEFAULT_SELL_IN = 10
+DEFAULT_QUALITY = 50
+@pytest.fixture
+def sulfura():
+    return Item(SULFURAS, DEFAULT_SELL_IN, DEFAULT_QUALITY)
+
+
+def test_given_a_sulfura_item_when_update_quality_then_it_should_keep_the_same_quality_as_before(sulfura):
+    gilded_rose = GildedRose([sulfura])
+    gilded_rose.update_quality()
+    assert gilded_rose.items[0].quality == DEFAULT_QUALITY
