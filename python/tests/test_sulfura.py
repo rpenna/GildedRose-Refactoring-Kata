6988986fe7a1307462d2ab86231e11ec4493b8e5
Rafael Penna <rafaelgpenna@gmail.com>
2023-09-10 12:34:22 -0300

    test: fix sulfuras default quality

--- python/tests/test_sulfura.py
+++ python/tests/test_sulfura.py
@@ -3,20 +3,25 @@ from gilded_rose import Item, GildedRose, SULFURAS
 
 
 DEFAULT_SELL_IN = 10
-DEFAULT_QUALITY = 50
+DEFAULT_QUALITY = 80
+
 
 @pytest.fixture
 def sulfura():
     return Item(SULFURAS, DEFAULT_SELL_IN, DEFAULT_QUALITY)
 
 
-def test_given_a_sulfura_item_when_update_quality_then_it_should_keep_the_same_quality_as_before(sulfura):
+def test_given_a_sulfura_item_when_update_quality_then_it_should_keep_the_same_quality_as_before(
+    sulfura,
+):
     gilded_rose = GildedRose([sulfura])
     gilded_rose.update_quality()
     assert gilded_rose.items[0].quality == DEFAULT_QUALITY
 
 
-def test_given_a_sulfura_item_when_update_quality_then_the_sell_in_should_keep_the_same(sulfura):
+def test_given_a_sulfura_item_when_update_quality_then_the_sell_in_should_keep_the_same(
+    sulfura,
+):
     gilded_rose = GildedRose([sulfura])
     gilded_rose.update_quality()
     assert gilded_rose.items[0].sell_in == DEFAULT_SELL_IN
