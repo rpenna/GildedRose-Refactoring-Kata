8f8bdba71ca0e9292cf599f73f46e44a3501d797
Rafael Penna <rafaelgpenna@gmail.com>
2023-09-09 21:14:17 -0300

    test: add min quality test for default item

--- python/tests/test_default_item.py
+++ python/tests/test_default_item.py
@@ -5,18 +5,25 @@ from gilded_rose import GildedRose, Item
 ITEM_NAME = "foo"
 MAX_QUALITY = 50
 MIN_QUALITY = 0
-DEFAULT_SELL_IN = 10
+MAX_SELL_IN = 10
 
 
 def test_given_a_max_quality_and_large_sell_in_item_when_update_quality_then_it_should_decrease_quality_by_1():
-    item = Item(ITEM_NAME, DEFAULT_SELL_IN, MAX_QUALITY)
+    item = Item(ITEM_NAME, MAX_SELL_IN, MAX_QUALITY)
     gilded_rose = GildedRose([item])
     gilded_rose.update_quality()
     assert gilded_rose.items[0].quality == MAX_QUALITY - 1
 
 
-def test_given_an_item_when_update_quality_then_it_should_decrease_sell_in():
-    item = Item(ITEM_NAME, DEFAULT_SELL_IN, MAX_QUALITY)
+def test_given_a_min_quality_and_large_sell_in_item_when_update_quality_then_it_should_keep_the_same_quality_as_before():
+    item = Item(ITEM_NAME, MAX_SELL_IN, MIN_QUALITY)
     gilded_rose = GildedRose([item])
     gilded_rose.update_quality()
-    assert gilded_rose.items[0].sell_in == DEFAULT_SELL_IN - 1
+    assert gilded_rose.items[0].quality == MIN_QUALITY
+
+
+def test_given_an_item_when_update_quality_then_it_should_decrease_sell_in():
+    item = Item(ITEM_NAME, MAX_SELL_IN, MAX_QUALITY)
+    gilded_rose = GildedRose([item])
+    gilded_rose.update_quality()
+    assert gilded_rose.items[0].sell_in == MAX_SELL_IN - 1
