b3cc270a0a2d0e1b7b21e9c9db5ed3e271bdd821
Rafael Penna <rafaelgpenna@gmail.com>
2023-09-09 21:20:45 -0300

    test: fix test functions names

--- python/tests/test_default_item.py
+++ python/tests/test_default_item.py
@@ -16,14 +16,14 @@ def test_given_a_max_quality_and_large_sell_in_item_when_update_quality_then_it_
     assert gilded_rose.items[0].quality == MAX_QUALITY - 1
 
 
-def test_given_a_negative_sell_in_item_when_update_quality_then_it_should_decrease_quality_by_2():
+def test_given_a_min_quality_item_when_update_quality_then_it_should_keep_the_same_quality_as_before():
     item = Item(ITEM_NAME, MAX_SELL_IN, MIN_QUALITY)
     gilded_rose = GildedRose([item])
     gilded_rose.update_quality()
     assert gilded_rose.items[0].quality == MIN_QUALITY
 
 
-def test_given_a_max_quality_and_large_sell_in_item_when_update_quality_then_it_should_decrease_quality_by_1():
+def test_given_a_negative_sell_in_item_when_update_quality_then_it_should_decrease_quality_by_2():
     item = Item(ITEM_NAME, NEGATIVE_SELL_IN, MAX_QUALITY)
     gilded_rose = GildedRose([item])
     gilded_rose.update_quality()
