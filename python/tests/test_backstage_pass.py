05c25f268a111b909edd0cd56b9781b131663e96
Rafael Penna <rafaelgpenna@gmail.com>
2023-09-10 14:10:59 -0300

    test: add sell in assert for backstage pass with medium term sell in test

--- python/tests/test_backstage_pass.py
+++ python/tests/test_backstage_pass.py
@@ -18,11 +18,25 @@ def test_given_a_long_term_sell_in_backstage_pass_when_update_quality_then_it_sh
     assert gilded_rose.items[0].quality == DEFAULT_QUALITY + 1
 
 
+def test_given_a_long_term_sell_in_backstage_pass_when_update_quality_then_it_should_decresase_sell_in_by_1():
+    backstage_pass = Item(BACKSTAGE_PASS, LONG_TERM_SELL_IN, DEFAULT_QUALITY)
+    gilded_rose = GildedRose([backstage_pass])
+    gilded_rose.update_quality()
+    assert gilded_rose.items[0].sell_in == LONG_TERM_SELL_IN - 1
+
+
 def test_given_a_long_term_sell_in_with_max_quality_backstage_pass_when_update_quality_then_quality_should_not_change():
     backstage_pass = Item(BACKSTAGE_PASS, LONG_TERM_SELL_IN, MAX_QUALITY)
     gilded_rose = GildedRose([backstage_pass])
     gilded_rose.update_quality()
     assert gilded_rose.items[0].quality == MAX_QUALITY
+
+
+def test_given_a_medium_term_sell_in_backstage_pass_when_update_quality_then_it_should_decresase_sell_in_by_1():
+    backstage_pass = Item(BACKSTAGE_PASS, MEDIUM_TERM_SELL_IN, DEFAULT_QUALITY)
+    gilded_rose = GildedRose([backstage_pass])
+    gilded_rose.update_quality()
+    assert gilded_rose.items[0].sell_in == MEDIUM_TERM_SELL_IN - 1
 
 
 def test_given_a_medium_term_sell_in_backstage_pass_when_update_quality_then_it_should_increase_quality_by_2():
