b404ede3e0d10abb38380cab195dc02f4dd7e59d
Rafael Penna <rafaelgpenna@gmail.com>
2023-09-10 14:12:07 -0300

    test: add sell in assert for backstage pass with short term sell in test

--- python/tests/test_backstage_pass.py
+++ python/tests/test_backstage_pass.py
@@ -60,6 +60,13 @@ def test_given_a_short_term_sell_in_backstage_pass_when_update_quality_then_it_s
     assert gilded_rose.items[0].quality == DEFAULT_QUALITY + 3
 
 
+def test_given_a_short_term_sell_in_backstage_pass_when_update_quality_then_it_should_decresase_sell_in_by_1():
+    backstage_pass = Item(BACKSTAGE_PASS, SHORT_TERM_SELL_IN, DEFAULT_QUALITY)
+    gilded_rose = GildedRose([backstage_pass])
+    gilded_rose.update_quality()
+    assert gilded_rose.items[0].sell_in == SHORT_TERM_SELL_IN - 1
+
+
 def test_given_a_short_term_sell_in_with_max_quality_backstage_pass_when_update_quality_then_quality_should_not_change():
     backstage_pass = Item(BACKSTAGE_PASS, SHORT_TERM_SELL_IN, MAX_QUALITY)
     gilded_rose = GildedRose([backstage_pass])
