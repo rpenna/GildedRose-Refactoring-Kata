ab5c95b8f1a9d2d9b420d8f43852e8dbe4884457
Rafael Penna <rafaelgpenna@gmail.com>
2023-09-11 10:32:02 -0300

    refact: create class inheritance structure

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -1,18 +1,42 @@
 # -*- coding: utf-8 -*-
+###############################
+#           ToDos
+# 1. Add new item "Conjured"
+# 2. Update README.md
+#
+###############################
+
+
+from typing import Protocol
+
 
 AGED_BRIE = "Aged Brie"
 BACKSTAGE_PASS = "Backstage passes to a TAFKAL80ETC concert"
 SULFURAS = "Sulfuras, Hand of Ragnaros"
 
 
-class GildedRose(object):
-    def __init__(self, items):
-        self.items = items
+class Item:
+    def __init__(self, name, sell_in, quality):
+        self.name = name
+        self.sell_in = sell_in
+        self.quality = quality
 
-    def __update_sulfuras(self):
-        return
+    def __repr__(self):
+        return f"{self.name}, {self.sell_in}, {self.quality}"
 
-    def __update_backstage_pass(self, item):
+
+class Updater(Protocol):
+    def update_quality(self, item: Item) -> Item:
+        pass
+
+
+class UpdaterSulfuras:
+    def update_quality(self, item: Item) -> Item:
+        return item
+
+
+class UpdaterBackstagePass:
+    def update_quality(self, item: Item) -> Item:
         if item.quality < 50:
             item.quality = item.quality + 1
             if item.sell_in < 11:
@@ -24,16 +48,22 @@ class GildedRose(object):
         item.sell_in = item.sell_in - 1
         if item.sell_in < 0:
             item.quality = 0
+        return item
 
-    def __update_aged_brie(self, item):
+
+class UpdaterAgedBrie:
+    def update_quality(self, item: Item) -> Item:
         if item.quality < 50:
             item.quality = item.quality + 1
         if item.sell_in < 0:
             if item.quality < 50:
                 item.quality = item.quality + 1
         item.sell_in = item.sell_in - 1
+        return item
 
-    def __update_default(self, item):
+
+class UpdaterDefaultItem:
+    def update_quality(self, item: Item) -> Item:
         if item.quality > 0:
             item.quality = item.quality - 1
         item.sell_in = item.sell_in - 1
@@ -41,26 +71,18 @@ class GildedRose(object):
             return
         if item.quality > 0:
             item.quality = item.quality - 1
+        return item
+
+
+class GildedRose(object):
+    def __init__(self, items):
+        self.items = items
+        self.__items_updaters = dict()
+        self.__items_updaters[SULFURAS] = UpdaterSulfuras
+        self.__items_updaters[AGED_BRIE] = UpdaterAgedBrie
+        self.__items_updaters[BACKSTAGE_PASS] = UpdaterBackstagePass
 
     def update_quality(self):
         for item in self.items:
-            if item.name == SULFURAS:
-                self.__update_sulfuras()
-                continue
-            if item.name == AGED_BRIE:
-                self.__update_aged_brie(item)
-                continue
-            if item.name == BACKSTAGE_PASS:
-                self.__update_backstage_pass(item)
-                continue
-            self.__update_default(item)
-
-
-class Item:
-    def __init__(self, name, sell_in, quality):
-        self.name = name
-        self.sell_in = sell_in
-        self.quality = quality
-
-    def __repr__(self):
-        return f"{self.name}, {self.sell_in}, {self.quality}"
+            updater = self.__items_updaters.get(item.name, UpdaterDefaultItem)()
+            item = updater.update_quality(item)
