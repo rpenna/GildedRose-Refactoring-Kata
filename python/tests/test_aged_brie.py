c3ceedb3a3df6939165738e98683c9f8166658d6
Rafael Penna <rafaelgpenna@gmail.com>
2023-09-10 21:40:25 -0300

    test: add test for expiring sell in aged brie

--- python/tests/test_aged_brie.py
+++ python/tests/test_aged_brie.py
@@ -5,6 +5,7 @@ from gilded_rose import AGED_BRIE, GildedRose, Item
 DEFAULT_QUALITY = 10
 MAX_QUALITY = 50
 DEFAULT_SELL_IN = 10
+ZERO_SELL_IN = 0
 
 
 @pytest.fixture
@@ -31,3 +32,10 @@ def test_given_a_max_quality_aged_brie_item_when_update_quality_then_it_should_k
     gilded_rose = GildedRose([max_quality_aged_brie])
     gilded_rose.update_quality()
     assert gilded_rose.items[0].quality == MAX_QUALITY
+
+
+def test_given_a_zero_sell_in_aged_brie_when_update_quality_then_it_should_increase_quality_by_1():
+    aged_brie = Item(AGED_BRIE, 0, DEFAULT_QUALITY)
+    gilded_rose = GildedRose([aged_brie])
+    gilded_rose.update_quality()
+    assert gilded_rose.items[0].quality == DEFAULT_QUALITY + 1
