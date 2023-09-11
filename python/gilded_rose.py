46269aab13b72b7c067c53dc0a787beb6bb8740e
Rafael Penna <rafaelgpenna@gmail.com>
2023-09-10 21:13:13 -0300

    refact: make update_quality method more readable

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -6,39 +6,39 @@ SULFURAS = "Sulfuras, Hand of Ragnaros"
 
 
 class GildedRose(object):
-
     def __init__(self, items):
         self.items = items
 
     def update_quality(self):
         for item in self.items:
-            if item.name != AGED_BRIE and item.name != BACKSTAGE_PASS:
-                if item.quality > 0:
-                    if item.name != SULFURAS:
-                        item.quality = item.quality - 1
-            else:
+            if item.name == SULFURAS:
+                continue
+            if item.name == AGED_BRIE:
                 if item.quality < 50:
                     item.quality = item.quality + 1
-                    if item.name == BACKSTAGE_PASS:
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != SULFURAS:
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != AGED_BRIE:
-                    if item.name != BACKSTAGE_PASS:
-                        if item.quality > 0:
-                            if item.name != SULFURAS:
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
+                if item.sell_in < 0:
                     if item.quality < 50:
                         item.quality = item.quality + 1
+            elif item.name == BACKSTAGE_PASS:
+                if item.quality < 50:
+                    item.quality = item.quality + 1
+                    if item.sell_in < 11:
+                        if item.quality < 50:
+                            item.quality = item.quality + 1
+                    if item.sell_in < 6:
+                        if item.quality < 50:
+                            item.quality = item.quality + 1
+            else:
+                if item.quality > 0:
+                    item.quality = item.quality - 1
+            item.sell_in = item.sell_in - 1
+            if item.sell_in >= 0:
+                continue
+            if item.name == BACKSTAGE_PASS:
+                item.quality = 0
+            else:
+                if item.quality > 0:
+                    item.quality = item.quality - 1
 
 
 class Item:
