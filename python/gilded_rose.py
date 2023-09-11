08def7acb86ec0be3669531ce3b4a7d20be2fed5
Rafael Penna <rafaelgpenna@gmail.com>
2023-09-10 21:30:10 -0300

    refact: create method for backstage pass update

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -12,6 +12,19 @@ class GildedRose(object):
     def __update_sulfuras(self):
         return
 
+    def __update_backstage_pass(self, item):
+        if item.quality < 50:
+            item.quality = item.quality + 1
+            if item.sell_in < 11:
+                if item.quality < 50:
+                    item.quality = item.quality + 1
+            if item.sell_in < 6:
+                if item.quality < 50:
+                    item.quality = item.quality + 1
+        item.sell_in = item.sell_in - 1
+        if item.sell_in < 0:
+            item.quality = 0
+
     def update_quality(self):
         for item in self.items:
             if item.name == SULFURAS:
@@ -24,22 +37,14 @@ class GildedRose(object):
                     if item.quality < 50:
                         item.quality = item.quality + 1
             elif item.name == BACKSTAGE_PASS:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.sell_in < 11:
-                        if item.quality < 50:
-                            item.quality = item.quality + 1
-                    if item.sell_in < 6:
-                        if item.quality < 50:
-                            item.quality = item.quality + 1
+                self.__update_backstage_pass(item)
+                continue
             else:
                 if item.quality > 0:
                     item.quality = item.quality - 1
             item.sell_in = item.sell_in - 1
             if item.sell_in >= 0:
                 continue
-            if item.name == BACKSTAGE_PASS:
-                item.quality = 0
             else:
                 if item.quality > 0:
                     item.quality = item.quality - 1
