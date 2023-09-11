994b2a0c0bd94f5a052c1c4d2cef2b6bcecc9d4f
Rafael Penna <rafaelgpenna@gmail.com>
2023-09-10 21:45:23 -0300

    refact: create method for default item update

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -33,6 +33,16 @@ class GildedRose(object):
                 item.quality = item.quality + 1
         item.sell_in = item.sell_in - 1
 
+    def __update_default(self, item):
+        if item.quality > 0:
+            item.quality = item.quality - 1
+        item.sell_in = item.sell_in - 1
+        if item.sell_in >= 0:
+            return
+        else:
+            if item.quality > 0:
+                item.quality = item.quality - 1
+
     def update_quality(self):
         for item in self.items:
             if item.name == SULFURAS:
@@ -44,14 +54,7 @@ class GildedRose(object):
             elif item.name == BACKSTAGE_PASS:
                 self.__update_backstage_pass(item)
                 continue
-            if item.quality > 0:
-                item.quality = item.quality - 1
-            item.sell_in = item.sell_in - 1
-            if item.sell_in >= 0:
-                continue
-            else:
-                if item.quality > 0:
-                    item.quality = item.quality - 1
+            self.__update_default(item)
 
 
 class Item:
