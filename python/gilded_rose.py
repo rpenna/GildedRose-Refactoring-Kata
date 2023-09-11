c6fd8f50af6b290be43e57e803261c21788f8f2d
Rafael Penna <rafaelgpenna@gmail.com>
2023-09-10 21:43:42 -0300

    refact: create method for aged brie update

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -25,25 +25,27 @@ class GildedRose(object):
         if item.sell_in < 0:
             item.quality = 0
 
+    def __update_aged_brie(self, item):
+        if item.quality < 50:
+            item.quality = item.quality + 1
+        if item.sell_in < 0:
+            if item.quality < 50:
+                item.quality = item.quality + 1
+        item.sell_in = item.sell_in - 1
+
     def update_quality(self):
         for item in self.items:
             if item.name == SULFURAS:
                 self.__update_sulfuras()
                 continue
             if item.name == AGED_BRIE:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                if item.sell_in < 0:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
-                item.sell_in = item.sell_in - 1
+                self.__update_aged_brie(item)
                 continue
             elif item.name == BACKSTAGE_PASS:
                 self.__update_backstage_pass(item)
                 continue
-            else:
-                if item.quality > 0:
-                    item.quality = item.quality - 1
+            if item.quality > 0:
+                item.quality = item.quality - 1
             item.sell_in = item.sell_in - 1
             if item.sell_in >= 0:
                 continue
