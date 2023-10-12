d780dd9287b1832f9f613c317cb56fb15e45ad4a
Rafael Penna <rafaelgpenna@gmail.com>
2023-10-12 20:28:08 -0300

    refact: improve backstage pass update_quality method readability

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -39,15 +39,14 @@ class UpdaterSulfuras:
 
 class UpdaterBackstagePass:
     def update_quality(self, item: Item) -> Item:
-        if item.quality < 50:
-            item.quality = item.quality + 1
-            if item.sell_in < 11:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-            if item.sell_in < 6:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-        item.sell_in = item.sell_in - 1
+        if item.sell_in < 6:
+            item.quality += 3
+        elif item.sell_in < 11:
+            item.quality += 2
+        else:
+            item.quality += 1
+        item.quality = min(item.quality, 50)
+        item.sell_in -= 1
         if item.sell_in < 0:
             item.quality = 0
         return item
