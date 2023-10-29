9ee6663fb210f4b0db70e59b83c68838b25aade3
Rafael Penna <rafaelgpenna@gmail.com>
2023-10-29 18:55:36 -0300

    test: add test when conjured item reaches min quality

--- python/tests/test_conjured.py
+++ python/tests/test_conjured.py
@@ -11,3 +11,10 @@ def test_given_a_non_min_quality_conjured_item_when_update_sell_in_then_it_shoul
     gilded_rose = GildedRose([item])
     gilded_rose.update_quality()
     assert gilded_rose.items[0].quality == MAX_QUALITY - 2
+
+
+def test_given_a_min_quality_conjured_item_when_update_sell_in_then_it_should_decrease_quality_by_2():
+    item = Item(ITEM_NAME, DEFAULT_SELL_IN, MIN_QUALITY)
+    gilded_rose = GildedRose([item])
+    gilded_rose.update_quality()
+    assert gilded_rose.items[0].quality == MIN_QUALITY
